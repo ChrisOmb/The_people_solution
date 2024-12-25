d36a05138b81c9fbe97fd3bb5c0b121854f1d484
ChrisOmb <christopherombwori06@gmail.com>
2024-12-25 22:48:55 +0300

    homepage update and tried static but failed

--- The_people/settings.py
+++ The_people/settings.py
@@ -124,9 +124,7 @@ STATIC_URL = '/static/'
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'polls', 'static',
-    ]
+STATICFILES_DIRS = [BASE_DIR/'static']
 
 
 
